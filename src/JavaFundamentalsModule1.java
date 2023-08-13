5a74ad6cb54b19f8ebbbccb0627e1cde3845f641
vinia <viniusadams98@gmail.com>
2023-08-14 00:02:59 +0100

    starting new exercises from the scracth

---
+++ src/JavaFundamentalsModule1.java
@@ -0,0 +1,13 @@
+/*Your challenge is to create four new variables:
+A byte variable, set it to any valid byte number, it doesn't matter.
+A short variable, set it to any valid short number.
+An int variable,  set it to any valid integer number.
+Lastly, create a variable of type long. Make it equal to 50,000 plus 10 times the sum of the values of the first 3 variables (your byte, your short and your int values).  In other words, use the variable names in your expression to calculate the sum.
+*/
+public class JavaFundamentalsModule1 {
+    byte element1 = 2;
+    short element = 3;
+    int element3 = 1;
+
+
+}
