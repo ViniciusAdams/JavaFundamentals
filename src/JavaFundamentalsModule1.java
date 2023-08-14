ace92f35e057d5a826f1f2e4a8e4202dd2332278
vinia <viniusadams98@gmail.com>
2023-08-14 22:13:17 +0100

    finishing first practical exercise from the 3th module

--- src/JavaFundamentalsModule1.java
+++ src/JavaFundamentalsModule1.java
@@ -5,9 +5,12 @@ An int variable,  set it to any valid integer number.
 Lastly, create a variable of type long. Make it equal to 50,000 plus 10 times the sum of the values of the first 3 variables (your byte, your short and your int values).  In other words, use the variable names in your expression to calculate the sum.
 */
 public class JavaFundamentalsModule1 {
+public static void main (String [] args){
     byte element1 = 2;
     short element = 3;
     int element3 = 1;
 
-
+long result = 50_000 + 10 * (element + element1 + element3);
+System.out.print ("Result=" + result);
+}
 }
