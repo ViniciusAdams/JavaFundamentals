869bb2e077db1bbb9fb519031b26e3dd08d4caae
vinia <viniusadams98@gmail.com>
2023-08-23 09:51:44 +0100

    finishing practice example inside the exercise

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -15,10 +15,29 @@ Finally, we'll call both methods and display the results for the following score
 
 public class Exercise3 {
     public static void main(String[] args) {
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Bob",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Percy",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Gilbert",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("James",highScorePosition);
+
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
-        System.out.println(playerName + "MANAGED TO GET INTO POSITION" + highScorePosition + "on the high score list");
+
+
+        System.out.println(playerName + " MANAGED TO GET INTO POSITION " + highScorePosition + " on the high score list");
+
+
     }
 
 
