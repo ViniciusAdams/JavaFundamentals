be3b5fe5e69aefa4b0cae4049b15dd777deae3f6
vinia <viniusadams98@gmail.com>
2023-09-06 23:36:32 +0100

    passing the return from calculateScore to a new int and then multiplying it by 1000 and printing it

--- src/MethodOverLoading/MethodOverloading.java
+++ src/MethodOverLoading/MethodOverloading.java
@@ -1,2 +1,16 @@
-package MethodOverLoading;public class MethodOverloading {
-}
+package MethodOverLoading;
+//https://www.udemy.com/course/java-the-complete-java-developer-course/learn/lecture/34998720#content
+public class MethodOverloading {
+    public static void main(String[] args) {
+   int newScore = calculateScore("Vini", 500);
+    System.out.println ("New score is" + newScore);
+    //passing return to int
+    }
+
+    public static int calculateScore(String playerName, int score) {
+
+        System.out.println("Player" + playerName + "scored" + score + "points");
+        return score * 1000;
+    }
+    }
+
