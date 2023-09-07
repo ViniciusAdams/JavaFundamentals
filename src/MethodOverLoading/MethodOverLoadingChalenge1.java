2c2c54c19624e6fd2c1f06220cf7780afd3f63d5
vinia <viniusadams98@gmail.com>
2023-09-07 23:28:31 +0100

    starting with the system.out

--- src/MethodOverLoading/MethodOverLoadingChalenge1.java
+++ src/MethodOverLoading/MethodOverLoadingChalenge1.java
@@ -4,4 +4,8 @@
 package MethodOverLoading;
 
 public class MethodOverLoadingChalenge1 {
+public static void main (String []args){
+    System.out.println("5ft, 8in = " + convertToCentimeters (5,8)+"cm");
+    System.out.println ("68in =" + convertToCentimeters(68)+"cm");
+}
 }
