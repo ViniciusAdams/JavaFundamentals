48fe31bdbf7505565c45cccf833a6354092971bb
vinia <viniusadams98@gmail.com>
2023-09-10 22:30:04 +0100

    adding second method

--- src/MethodOverLoading/SecodsToMinutesChalange.java
+++ src/MethodOverLoading/SecodsToMinutesChalange.java
@@ -3,26 +3,28 @@ package MethodOverLoading;
 class SecodsToMinutesChallenge {
     public static void main (String [] args) {
         System.out.println (getDurationString(3945));
+        System.out.println(getDurationString(65,45));
+
     }
+
 public static String getDurationString (int seconds){
    //two step approach to get the hours
     int minutes = seconds/60;
     int hours = minutes /60;
-    System.out.println ("hours = " + hours );
     int remainingMinutes = minutes % 60;
-    System.out.println("minutes = " + minutes);
-    System.out.println ("remainingMinutes = " + remainingMinutes );
     int remainingSeconds = seconds % 60;
-    System.out.println ("remainingSeconds = " + remainingSeconds);
-
-
     //One step approach
     int hours1 = seconds / 3600;
-    System.out.println ("hours1 ="  + hours1);
     return hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
 }
 public static String getDurationString (int minutes,int seconds){
-    return "";
+
+    int hours = minutes /60;
+    int remainingMinutes = minutes % 60;
+    int remainingSeconds = seconds % 60;
+    //One step approach
+    int hours1 = seconds / 3600;
+    return hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
 }
 
 }
