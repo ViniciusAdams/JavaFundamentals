5c7cfabc88d2eaedb27be933d17e580f8b8c7623
vinia <viniusadams98@gmail.com>
2023-09-10 20:33:50 +0100

    finishing first method.

--- src/MethodOverLoading/SecodsToMinutesChalange.java
+++ src/MethodOverLoading/SecodsToMinutesChalange.java
@@ -9,11 +9,17 @@ public static String getDurationString (int seconds){
     int minutes = seconds/60;
     int hours = minutes /60;
     System.out.println ("hours = " + hours );
+    int remainingMinutes = minutes % 60;
+    System.out.println("minutes = " + minutes);
+    System.out.println ("remainingMinutes = " + remainingMinutes );
+    int remainingSeconds = seconds % 60;
+    System.out.println ("remainingSeconds = " + remainingSeconds);
+
 
     //One step approach
     int hours1 = seconds / 3600;
     System.out.println ("hours1 ="  + hours1);
-    return "";
+    return hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
 }
 public static String getDurationString (int minutes,int seconds){
     return "";
