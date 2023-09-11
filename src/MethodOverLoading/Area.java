aa904999c9a1e0ce988b5445b8f2aff2f93356df
vinia <viniusadams98@gmail.com>
2023-09-11 23:28:48 +0100

    area calculator

---
+++ src/MethodOverLoading/Area.java
@@ -0,0 +1,22 @@
+package MethodOverLoading;
+
+public class Area {public class AreaCalculator {
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1.0; // Invalid value
+        } else {
+            return radius * radius * Math.PI; // Calculate circle area
+        }
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1.0; // Invalid value
+        } else {
+            return x * y; // Calculate rectangle area
+        }
+    }
+}
+
+
+}
