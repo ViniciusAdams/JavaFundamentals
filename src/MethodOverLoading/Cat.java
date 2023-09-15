189bf839b25966e3334bdd33618f34a0ec4bd932
vinia <viniusadams98@gmail.com>
2023-09-15 18:51:01 +0100

    commiting

--- src/MethodOverLoading/Cat.java
+++ src/MethodOverLoading/Cat.java
@@ -2,7 +2,7 @@ package MethodOverLoading;
 
 public class Cat {
     public static boolean isCatPlaying(boolean summer, int temperature) {
-        int upperLimit = (summer) ? 45 : 35;
+        int upperLimit = (summer) ? 45 : 35;git
 
         return temperature >= 25 && temperature <= upperLimit;
     }
