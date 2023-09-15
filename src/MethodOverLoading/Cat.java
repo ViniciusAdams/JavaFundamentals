fd61df2453a1de9dfc0fb1466b184bef227e0b27
vinia <viniusadams98@gmail.com>
2023-09-15 18:54:19 +0100

    adding new class

--- src/MethodOverLoading/Cat.java
+++ src/MethodOverLoading/Cat.java
@@ -2,7 +2,7 @@ package MethodOverLoading;
 
 public class Cat {
     public static boolean isCatPlaying(boolean summer, int temperature) {
-        int upperLimit = (summer) ? 45 : 35;git
+        int upperLimit = (summer) ? 45 : 35;
 
         return temperature >= 25 && temperature <= upperLimit;
     }
