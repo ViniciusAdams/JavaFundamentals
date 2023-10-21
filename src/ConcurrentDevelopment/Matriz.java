0ee13c53b7d07e9550c6b72c8c6b69f450ee0162
vinia <viniusadams98@gmail.com>
2023-10-21 23:44:07 +0100

    add

--- src/ConcurrentDevelopment/Matriz.java
+++ src/ConcurrentDevelopment/Matriz.java
@@ -1,112 +1,112 @@
-package ConcurrentDevelopment;
-
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+;
+public class Question1 {
+    public static void main(String[] args) {
+        char[][] m = generateRandomMatrix(1000, 1000); // Genarating a 1000x1000
+        String word = "fun";
+        int threadC = 6;
+        ExecutorService es = Executors.newFixedThreadPool(threadC);
+        List<Search> tasks = new ArrayList<>();
+        // Creating tasks to different directions
+        tasks.add(new Search(m, word.toCharArray(), D.H));
+        tasks.add(new Search(m, word.toCharArray(), D.V));
+        tasks.add(new Search(m, word.toCharArray(), D.DF));
+        tasks.add(new Search(m, word.toCharArray(), D.DR));
+        int Count = 0;
+        long sTime = System.currentTimeMillis();
 
-public class Matriz {
-    public static void main (String[] args){
-        char [][] m = {
+        try {
+            List<Future<Integer>> results = es.invokeAll(tasks);
+            for (Future<Integer> result : results) {
+                Count += result.get();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
 
-        };
+        } finally {
+            es.shutdown();
+
+        }
+
+
+        long aendTime = System.currentTimeMillis();
+
+        long eTime = aendTime - sTime;
+        System.out.println("Count=" +Count);
+        System.out.println("Execution-Time:" +eTime +" ms");
     }
-    String word = "fun";
-    int threadC = Runtime.getRuntime().availableProcessors();
-    ExecutorService es = Executors.newFixedThreadPool(threadC);
-    //Creating an Executor with a pool
-    private static class Search implements Callable <Integer> {
-        private char [][] m ;
-        private String t;
-        private int sRow;
-        private int eRow;
-        private int Count;
-
-        @Override
-        public Integer call() {
-            int count = 0;
-
-            for (int i = sRow; i < eRow; i++) {
-                for (int j = 0; j < Count; j++) {
-                    // Horizontal search
-                    if (Horizontal(i, j)) {
-                        count++;
-                    }
-                    // Vertical search
-                    if (Vertical(i, j)) {
-                        count++;
-                    }
-                    // Diagonal search (left to right)
-                    if (DiagonalL(i, j)) {
-                        count++;
-                    }
-                    // Diagonal search (right to left)
-                    if (DiagonalR(i, j)) {
-                        count++;
-                    }
-                }
+    //
+    private static char[][] generateRandomMatrix(int r, int c) {
+        Random randomGenerator = new Random();
+        char[][] arr = new char[r][c];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (char) (randomGenerator.nextInt(26) + 97);
             }
-
-            return count;
         }
-
-
-
-        private boolean Vertical(int row, int col) {
-            // checking verticaly
-            if (row + 2 >= eRow) {
-                return false;
-            }
-
-            for (int i = 0; i < 3; i++) {
-                if (m[row + i][col] != t.charAt(i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private boolean Horizontal(int row, int col) {
-            // Check horizontally
-            if (col + 2 >= Count) {
-                return false;
-            }
-
-            for (int i = 0; i < 3; i++) {
-                // Check "fun"
-                if (m[row][col + i] != t.charAt(i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private boolean DiagonalL(int row, int col) {
-            // checking left to right
-            if (col + 2 >= Count || row + 2 >= eRow) {
-                return false;
-            }
-            // fun?
-            for (int i = 0; i < 3; i++) {
-                if (m[row + i][col + i] != t.charAt(i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        private boolean DiagonalR(int row, int col) {
-            // Check if there is "fun" diagonally (right to left)
-            if (col - 2 < 0 || row + 2 >= eRow) {
-                return false;
-            }
-            // Check if the characters form "fun"
-
-            //check if all the letters make "fun"
-            for (int i = 0; i < 3; i ++){
-                if (m[row + i][col - i] != t.charAt(i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
+        return arr;
     }
 }
+//defining set of constants
+enum D {
+    H, V, DF, DR
+}
+class Search implements Callable<Integer> {
+    private char[][] m;
+    private char[] t;
+    private D location;
+    public Search(char[][] m, char[] t, D location) {
+        this.m = m;
+        this.t = t;
+        this.location = location;
+    }
+    public Integer call() {
+        int c = 0;
+        int rows = m.length;
+        int cols = m[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (checkWord(i, j, rows, cols)) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+    //METHOD TO CHECK THE WORDS
+    private boolean checkWord(int row, int col, int rows, int cols) {
+        for (int i = 0; i < t.length; i++) {
+            int r = row, c = col;
+
+            switch (location) {
+                case H:
+                    c += i;
+                    break;
+                case V:
+                    r += i;
+                    break;
+                case DF:
+                    r += i;
+                    c -= i;
+                    break;
+                case DR:
+                    r += i;
+                    c += i;
+                    break;
+            }
+            if (r < 0 || r >= rows || c < 0 || c >= cols || m[r][c] != t[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+//Vinicius Eduardo Adams de Castro#
+//3052655
