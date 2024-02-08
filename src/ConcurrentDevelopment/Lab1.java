f43b3179fae1235bd775b0b8ca86188a91448440
vinia <viniusadams98@gmail.com>
2024-02-08 00:01:15 +0000

    adding a few practice exercises

--- src/ConcurrentDevelopment/Lab1.java
+++ src/ConcurrentDevelopment/Lab1.java
@@ -3,32 +3,36 @@ package ConcurrentDevelopment;
 import java.util.Random;
 //Question 1
 //Write a thread that tosses a coin 1000 times and computes the frequency of heads and tails.
-class TossThread extends Thread{
-    private int n=0;
+public class Lab1 extends Thread{
 
-    public void run() {
-        for(int k = 0; k < 1000; k++)
-            if ((int)( Math.random()*2)==1) n++;
+    public static void main(String args[]) {
+    TossThread res =new TossThread();
+    res.start();
+    try{
+        res.join();
     }
-
-    public int getResult(){
-        return n;
+    catch(InterruptedException e){
 
     }
+    System.out.println("Head="+res.getResult());
+    System.out.println("Tail="+(100-res.getResult()));
 
-    class Lab1{
-        public static void main(String args[]){
-            TossThread res = new TossThread();
-            res.start();
-            try{
-                res.join();
-            }
-            catch(InterruptedException e){}
-            System.out.println("Head =  "+ res.getResult());
-            System.out.println("Tail =  "+ (1000 -res.getResult()));
-        }
     }
 }
+
+
+    class TossThread extends Thread{
+    public int n = 0;
+    public void run(){
+        for (int k = 0; k < 1000; k ++)
+            if ((int) (Math.random()* 2) ==1) n++;
+    }
+    public int getResult(){
+        return n;
+    }
+
+        }
+
 
 
 
--- src/ConcurrentDevelopment/Lab1Q2.java
+++ src/ConcurrentDevelopment/Lab1Q2.java
@@ -1,46 +1,28 @@
 package ConcurrentDevelopment;
 
-public class Lab1Q2 implements Runnable {
-    //create variable volatile boolean is running?
-    private volatile boolean isRunning = true;
+public class Lab1Q2 extends Thread {
+    class MessagePrinter extends Thread{
+        private volatile boolean go = true;
+        private Thread t;
+        public void run(){
+            while(go){
+                try{
+                    this.sleep(100);
+                }catch(InterruptedException e){}
+                t = Thread.currentThread();
 
-    @Override
-    //method runt
-    public void run() {
-       //while it is running print a message
-        while (isRunning) {
-            System.out.println("message");
-           //try thread.sleep each 100 milliseconds
-            try {
-                Thread.sleep(100); // Sleep for 100 milliseconds
-            //catch ifis Running is false print terminated
-            } catch (InterruptedException e) {
-                if (!isRunning) {
-                    System.out.println("Terminated");
-                    return;
-                }
+                if (t.isAlive())
+                    System.out.println("Running");
             }
         }
+        public void terminate()
+        {
+            go = false; }
     }
 
-    public void stopThread() {
-        //creating a method and setting it to false
-        isRunning = false;
+
+    //create variable volatile boolean is running?
+
+
     }
 
-    public static void main(String[] args) {
-        //creating method Lab1q2
-        Lab1Q2 lab1Q2 = new Lab1Q2();
-        //creating thread with method as an argument
-        Thread thread = new Thread(lab1Q2);
-        //thread start
-        thread.start();
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        lab1Q2.stopThread();
-    }
-}
